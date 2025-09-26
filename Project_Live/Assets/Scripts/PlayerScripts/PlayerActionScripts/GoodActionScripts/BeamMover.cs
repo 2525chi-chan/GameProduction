03e702e55f165ee6ebec09f05127de6b8a1bcf68
lastre1208 <lastobserver.1208@gmail.com>
2025-09-27 02:30:38 +0900

    ビームのひな型の完成

--- Project_Live/Assets/Scripts/PlayerScripts/PlayerActionScripts/GoodActionScripts/BeamMover.cs
+++ Project_Live/Assets/Scripts/PlayerScripts/PlayerActionScripts/GoodActionScripts/BeamMover.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.VFX;
 //作成者：桑原
 
 public class BeamMover : MonoBehaviour
@@ -12,17 +12,24 @@ public class BeamMover : MonoBehaviour
     [SerializeField] float maxLength = 10f;
     [Header("形状を変えるオブジェクト")]
     [SerializeField] Transform target;
-
+    [Header("ビームのエフェクト")]
+    [SerializeField]GameObject beamEffect;
     float currentLength = 0f;
     float previewLength = 0f;
     Vector3 initialScale;
-
+     VisualEffect vfx;
     void Start()
     {
         if (target == null) return;
-
+     GameObject effect= Instantiate(beamEffect, target.position, Quaternion.identity);
+         vfx= effect.GetComponent<VisualEffect>();
         initialScale = target.localScale;
         previewLength = initialScale.y;
+       
+        vfx.SetVector3("StartPos", target.position); 
+        
+        Quaternion forwardDir = target.rotation;
+        vfx.SetFloat("BeamDir", forwardDir.eulerAngles.y);
     }
 
 
@@ -40,5 +47,16 @@ public class BeamMover : MonoBehaviour
         float delta = currentLength - previewLength;
 
         target.localPosition = new Vector3(0, 0, (currentLength + delta) / 2);
+
+
+        Vector3 beamEnd = target.position + target.up * currentLength;
+
+        float beamScale = Vector3.Distance(vfx.GetVector3("StartPos"), beamEnd);
+
+        vfx.SetVector3("EndPos", target.position);
+        vfx.SetFloat("BeamScale", beamScale);
+
+
+       
     }
 }
